833f861df78226624bea64f74816f40febdb8a5e
Daniel van Balen <dvbalen@gmail.com>
2014-01-05 19:35:49 -0500

    Added __copy__ magic method; Fixed url generation when no parameters; Hid obj creation behind method plus code for requestHandler support

--- SimpleSolr.py
+++ SimpleSolr.py
@@ -1,14 +1,20 @@
 
 
+import copy
 import urllib.parse
 
-class SimpleSolr(object):
+def solr_select(url,path):
+  return _SimpleSolr(url,path,'solr','select','dismax')
 
-  def __init__(self,url,core,handler_path='solr'):
+class _SimpleSolr(object):
+
+  # @todo request_handler doesn't do anything
+  def __init__(self,url,core,handler_path='solr',handler_name='select',request_handler='dismax'):
     self.url = url.rstrip('/')
     self.core = core.rstrip('/')
     self.handler_path = handler_path.rstrip('/')
-    self.handler_name = 'select'
+    self.handler_name = handler_name
+    self.request_handler = request_handler
     self.data = {}
 
     def store_one(key,item):
@@ -19,15 +25,23 @@ class SimpleSolr(object):
         self.data[key] = list()
       self.data[key].append(item)
     
+    # @todo clean up, methods for dismax,edismax,et all
+    # @todo make plugable 
     self.methods = {
         'fq': lambda x: store_all('fq',x),
+        'q': lambda x: store_all('q',x),
         'rows': lambda x: store_one('rows',x),
+        'start': lambda x: store_one('start',x),
         }
     
 
-  def _store(self,name):
-    new_self = SimpleSolr(self.url,self.core,self.handler_path)
+  def __copy__(self):
+    new_self = _SimpleSolr(self.url,self.core,self.handler_path,self.handler_name,self.request_handler)
     new_self.data = dict(self.data)
+    return new_self
+
+  def _store(self,name):
+    new_self = copy.copy(self)
     def _ret(*args,**kwargs):
       new_self.methods[name](*args,**kwargs)
       return new_self
@@ -40,15 +54,16 @@ class SimpleSolr(object):
     raise AttributeError("'%s' has no attribute '%s'" % (self.__class__,name) )
 
   def get_query(self):
-    path = [ '/'.join([self.url,self.handler_path,self.core,self.handler_name]),'?' ]
+    path = [ '/'.join([self.url,self.handler_path,self.core,self.handler_name]), ]
     params = []
     for k,v in self.data.items():
       kv_lst = zip([k]*len(v),v)
       params.extend(l for l in kv_lst)
-    path.append(urllib.parse.urlencode(params))
+    if params:
+      path.extend(['?',urllib.parse.urlencode(params)])
     return ''.join(path)
 
-q = SimpleSolr('http://localhost:8983','core1')
+q = solr_select('http://localhost:8983','core1')
 
 if __name__ == "__main__":
   q = SimpleSolr('http://localhost:8983','core1')
